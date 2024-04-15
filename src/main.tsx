ccef85ec78469bf19ff5a9f9fabd4dab664d6476
867380699 <867380699@qq.com>
2024-04-15 09:47:01 +0800

    feat(android): status and nav colors

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { StatusBar } from "@capacitor/status-bar";
+import { NavigationBar } from "@capgo/capacitor-navigation-bar";
 
+StatusBar.setBackgroundColor({ color: "#585b70" });
+NavigationBar.setNavigationBarColor({ color: "#585b70" });
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
